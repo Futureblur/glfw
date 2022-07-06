cb049f1568ab0d1fcb42a1fb0e0cd118ed4a365a
Futureblur <itsfutureblur@gmail.com>
2022-07-06 13:58:21 +0200

    Update files for linux

--- premake5.lua
+++ premake5.lua
@@ -77,16 +77,28 @@ project "glfw"
 	filter "system:linux"
 		files
 		{
+			"src/x11_platform.h",
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
+			
+			"src/xkb_unicode.h",
 			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
+			
 			"src/glx_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+			
+			"src/linux_joystick.c",
+			"src/linux_joystick.h",
+			
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"src/posix_time.h",
+			"src/posix_thread.h",
+			"src/posix_poll.c",
+			"src/posix_poll.h"
 		}
 
 		defines "_GLFW_X11"
