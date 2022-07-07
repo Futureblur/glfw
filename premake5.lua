efac52199d879ec29edfeade27d4a0a5c0e69029
Futureblur <itsfutureblur@gmail.com>
2022-07-07 15:17:50 +0200

    Fix windows file paths

--- premake5.lua
+++ premake5.lua
@@ -58,22 +58,26 @@ project "glfw"
 	filter "system:windows"
 		files
 		{
-			"win32_time.h",
-			"win32_thread.h",
-			"win32_module.c",
-			"win32_time.c",
-			"win32_thread.c",
+			"src/win32_time.h",
+			"src/win32_thread.h",
+			"src/win32_module.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
 
-			"win32_platform.h",
-			"win32_joystick.h",
-			"win32_init.c",
-			"win32_joystick.c",
-			"win32_monitor.c",
-			"win32_window.c",
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_window.c",
 			"wgl_contect.c"
 		}
 
-		defines "_GLFW_WIN32"
+		defines 
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
 
 		systemversion "latest"
 		staticruntime "on"
