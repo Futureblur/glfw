f518230549a6053a81ce20c97210495ff00210ea
Futureblur <itsfutureblur@gmail.com>
2022-07-19 07:04:55 +0200

    Turn off `staticruntime` and use premake's variable for `/MDd`

--- premake5.lua
+++ premake5.lua
@@ -80,7 +80,13 @@ project "glfw"
 		}
 
 		systemversion "latest"
-		staticruntime "on"
+		staticruntime "off"
+
+	filter { "configurations:Debug", "system:windows" }
+		runtime "Debug"
+
+	filter { "configurations:Release", "system:windows" }
+		runtime "Release"
 
 	filter "system:linux"
 		files
